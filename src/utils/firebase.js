24cbc1f065bbeba53e7ae1140987b0d0253205e3
Katerina Taneva <katerina.tanewa@gmail.com>
2021-04-09 15:53:09 +0300

    firebase i and setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBjEF6JGCwYFgWHKOT-M_lZeQxRjKs5V38",
+    authDomain: "himalaya-salt-room.firebaseapp.com",
+    projectId: "himalaya-salt-room",
+    storageBucket: "himalaya-salt-room.appspot.com",
+    messagingSenderId: "882056572913",
+    appId: "1:882056572913:web:b229cd9a8e1c1f26139e50",
+    measurementId: "G-GK5YEE5TPK"
+  };
+
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+
+
+  firebase.auth().onAuthStateChanged((user) =>{
+    if(user){
+      console.log('Logged In');
+      console.log(user);
+    }else{
+      console.log('Logged Out');
+    }
+  })
+export default firebase;
+
+export const auth = firebase.auth();
