1a0f7b31936456d30688b1fc476f6409531f9cdc
Katerina Taneva <katerina.tanewa@gmail.com>
2021-04-11 02:58:24 +0300

    BookSaltRoom choice updated

--- src/components/BookSaltRoom/BookSaltRoom.js
+++ src/components/BookSaltRoom/BookSaltRoom.js
@@ -1,19 +1,28 @@
 import { Link } from 'react-router-dom';
+import './BookSaltRoom.css';
 
 const BookSaltRoom = () => {
     return (
-        <section >
+        <div className='book-salt-room-div'>
             <h1>Запазване на час</h1>
-            <article>
-                <img src='https://cdn0.iconfinder.com/data/icons/man-actions/234/mother-son-growing-old-together-007-512.png' />
-                <span>Солна стая за възрастни</span>
-            </article>
-            <article>
-                <img src="https://static.thenounproject.com/png/11573-200.png" />
-                <span>Солна стая за деца</span>
-            </article>
+            <section className="book-salt-room-wrapper">
 
-        </section>
+                <Link to='#' className="book-adults-room">
+                    <ul>
+                        <li><span>Солна стая за възрастни</span></li>
+                        <li><img src='https://cdn0.iconfinder.com/data/icons/man-actions/234/mother-son-growing-old-together-007-512.png' /></li>
+                    </ul>
+                </Link>
+                <Link to='#' className='book-childrens-room'>
+                    <ul>
+                        <li><span>Солна стая за деца</span></li>
+                        <li><img src="https://static.thenounproject.com/png/11573-200.png" /></li>
+                    </ul>
+
+                </Link>
+            </section>
+        </div>
     )
+
 }
 export default BookSaltRoom;
